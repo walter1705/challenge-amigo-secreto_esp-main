d14e2c43a63ad9bdc9eeeba2ececa1e92d4383db
walter1705 <134169546+walter1705@users.noreply.github.com>
2025-02-04 13:23:07 -0500

    Requisitos finalizados

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = [];
 
-function agregarAmigo(params) {
+
+function agregarAmigo() {
+    let amigoInput = document.querySelector('$amigo').value.trim();
     
+    if (amigoInput === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigoInput = "";
+    actualizarLista();    
+}
+
+function actualizarLista() {
+    let lista = document.querySelector(`$listaAmigos`);
+    lista.innerHTML = "";
+    for(let i = 0;i<listaAmigos.length;i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Agrega al menos un amigo antes de sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultadoPantalla = document.getElementById("resultado");
+    resultadoPantalla.innerHTML = `<li> El amigo secreto es: <strong>${amigoSorteado}</strong> </li>`;
 }
